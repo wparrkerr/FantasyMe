0b31c0a0a9e71b8e1801c119710232c29f385620
tomhuber <tomhuber@umich.edu>
2020-03-24 23:27:57 -0400

    added logout button and redirected login to goal viewing page

--- players-front/src/App.js
+++ players-front/src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
-import SignUpForm from "./components/SignUpForm";
 import Home from "./components/Home";
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.headerElement = React.createRef();
+    this.homeElement = React.createRef();
+  }
+
+  login_clicked = () => {
+    this.headerElement.current.update_logged_status();
+  }
+
+  logout_clicked = () => {
+    this.homeElement.current.setPage("landing");
+  }
+
   render() {
     return (
       <div>
-        <Header />
-        <Home />
-        {/* <SignUpForm /> */}
+        {/* source for "how to refer": https://www.freecodecamp.org/news/react-changing-state-of-child-component-from-parent-8ab547436271/ */}
+        <Header ref={this.headerElement} return_to_landing={this.logout_clicked}/>
+        <Home ref={this.homeElement} update_login_state={this.login_clicked}/>
       </div>
     );
   }
--- players-front/src/components/Header.js
+++ players-front/src/components/Header.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
+import { Button } from "reactstrap";
 import './styles/Header.css';
+import { isValid } from './helpers/token_helpers.js'
 
 class Header extends Component {
+
+  componentDidMount() {
+    this.update_logged_status();
+  }
+
+  update_logged_status = () => {
+    if (isValid(localStorage.getItem('access_token'))){ 
+      //isValid is a helper function (token_helpers.js)
+      document.getElementById("logged-status").style.display = "block";
+    } else {
+      document.getElementById("logged-status").style.display = "none";
+    }
+  }
+
+  logout_fun = e => {
+    e.preventDefault()
+    console.log("running logout_fun")
+    localStorage.setItem('access_token', "");
+    this.update_logged_status()
+    this.props.return_to_landing()
+  }
 
   render() {
     return (
       <div id = "outside">
         <div id = "text-div">
           <h1 id="header-text"><strong>Fantasy Me</strong></h1>
-          </div>
+        </div>
+        <Button size="sm" id="logged-status" color="primary" onClick={this.logout_fun}>
+          Log Out
+        </Button>
+        {/*localStorage.getItem('access_token') DEBUGGING OUTPUT */}
       </div>
     )
   }
+  
 
 }
 
